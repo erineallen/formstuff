376ba8ffe7d9584ade91b172bfaeaf4d08bf5245
erineallen <erin@erin-allen.com>
2024-04-25 12:25:38 -0400

    Update random-options.js

--- random-options.js
+++ random-options.js
@@ -23,3 +23,32 @@ document.addEventListener('DOMContentLoaded', function () {
         options.forEach(option => selectElement.appendChild(option));
     });
 });
+
+
+
+// Listen for change events and log the changes as they occur.
+form.on('formLoad', (form) => {
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    const selectElements = document.querySelectorAll('.randomize-select select');
+    const wrapper = document.querySelectorAll('.randomize-select');
+    wrapper.forEach(wrap => {
+        wrap.style.backgroundColor = 'lightblue'; 
+});
+
+    
+    selectElements.forEach(selectElement => {
+        const options = Array.from(selectElement.options);
+        shuffle(options);
+
+        while (selectElement.firstChild) {
+            selectElement.removeChild(selectElement.firstChild);
+        }
+        options.forEach(option => selectElement.appendChild(option));
+    });
+});
